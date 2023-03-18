68267325668ac61a343f83060905f615b797f5e2
Angrymouse <rykov.nikas@gmail.com>
2023-03-19 01:06:28 +0200

    add built files to repo

---
+++ .output/public/_nuxt/app.config.99ea903c.js
@@ -0,0 +1,1 @@
+import{D as n}from"./entry.87fa91ac.js";const i={};n(i);
---
+++ dist/_nuxt/login.a99b12d4.js
@@ -0,0 +1,1 @@
+import{s as r}from"./entry.87fa91ac.js";import"./nuxt-link.f89ff8b9.js";import"./app.config.99ea903c.js";const p={__name:"login",setup(t){return r().go(-1),(o,e)=>null}};export{p as default};
