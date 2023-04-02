5ed877c2b831c261b1c49759f21a2412e13e3ca0
Angrymouse <rykov.nikas@gmail.com>
2023-04-02 05:52:29 +0300

    add discovery page

---
+++ components/NftCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <NuxtLink :to="'/nft/' + nft.contractTxId"
+        class="nft-card card shadow-xl h-[20rem] w-[20rem] m-2  rounded-xl flex flex-col justify-end items-center " :style="{
+
+
+            backgroundRepeat: 'no-repeat',
+            backgroundClip: 'border-box',
+
+            backgroundColor: `black`,
+            backgroundImage: `radial-gradient(#00000030, #00000015), url('https://arweave.net/${nft.contractTxId}')`
+        }">
+
+
+        <div
+            class="rounded font-mono flex m-4 p-2 text-white bg-black bg-opacity-20 backdrop-blur-sm flex-col max-w-max justify-center items-center">
+
+            <h2 class="card-title">{{ nft.state.name }}</h2>
+            <p v-if="nft.state.description">{{ nft.state.description.length < 80 ? nft.state.description :
+                nft.state.description.slice(0, 77) + "..." }}</p>
+
+        </div>
+
+    </NuxtLink>
+</template>
+<style>
+.nft-card {
+    transition: all 0.3s ease-in-out;
+    background-size: cover;
+    transform: translate(0);
+    background-position: 50% 50%
+}
+
+.nft-card:hover {
+
+    transform: perspective(900px) translateY(-5%) rotateX(8deg) rotateY(-5deg) translateZ(0);
+    box-shadow: 2px 35px 35px -8px rgba(0, 0, 0, 0.75);
+    /* translate: 0 0 300px; */
+
+
+}
+</style>
+<script setup>
+const { nft } = defineProps(["nft"])
+console.log(nft)
+</script>
---
+++ pages/discover.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="h-full-navbared w-full flex flex-col items-center justify-center ">
+        <h1 class="font-mono text-2xl">Discover NFTs</h1>
+        <div class="flex flex-row flex-wrap justify-around">
+            <NftCard v-for="nft in nfts.result" :key="nft.contractTxId" :nft="nft"></NftCard>
+        </div>
+
+
+    </div>
+</template>
+
+<script setup>
+let nfts = await $fetch("https://prophet.rareweave.store/nfts")
+console.log(nfts)
+definePageMeta({
+    layout: "without-auth",
+});
+</script>
+<style>
+@keyframes amazing-bg {
+    from {
+
+        background-position-x: 0%;
+    }
+
+    50% {
+
+        background-position-x: 200%;
+    }
+
+    to {
+
+        background-position-x: 0%;
+    }
+}
+
+.amazing-button {
+    background: rgb(97, 9, 168);
+    background: radial-gradient(circle, rgba(97, 9, 168, 1) 0%, rgba(121, 41, 9, 1) 25%, rgba(179, 22, 83, 1) 50%, rgba(171, 49, 107, 1) 61%, rgba(167, 40, 83, 1) 73%, rgb(185, 25, 36) 100%);
+    background-size: 300% 300%;
+    animation: amazing-bg 10s linear infinite;
+}
+</style>
