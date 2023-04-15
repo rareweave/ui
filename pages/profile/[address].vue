8f0a73b9602d9484c56e621fda77ae3be96c537c
Ducksquaddd <ducksquadddd@gmail.com>
2023-04-15 08:43:51 -0700

    Fix collections not showing when logged in

--- pages/profile/[address].vue
+++ pages/profile/[address].vue
@@ -129,6 +129,15 @@
           :nft="nft"
         ></NftCard>
       </div>
+
+      <h2 class="text-center text-2xl mt-2 font-mono">Owned Collections:</h2>
+      <div class="flex flex-wrap justify-center flex-col">
+        <CollectionCard
+          v-for="collection in ownedCollections"
+          :key="collection.contractTxId"
+          :collection="collection"
+        ></CollectionCard>
+      </div>
     </template>
     <template v-else>
       <img
