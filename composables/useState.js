1440580c4d9a298177e8ee384ca5bf4c4721fec0
Angrymouse <rykov.nikas@gmail.com>
2023-05-24 23:39:50 +0300

    change gateway

--- composables/useState.js
+++ composables/useState.js
@@ -24,6 +24,13 @@ export const useAnsaddr = () => useState("ansaddr", () => null);
 export const useArweave = () => useState("arweave", () => null);
 
 export const useAccountTools = () => useState("accountTools", () => new Account({
+    gateway: {
+        host: "prophet.rareweave.store",
+        port: 443,
+        protocol: "https",
+        timeout: 60_000,
+        logging: false,
+    },
     cacheIsActivated: true,
     cacheSize: 100,
     cacheTime: 60,
