3f81033425e6492de642fbd6849196652c3ed738
Angrymouse <rykov.nikas@gmail.com>
2023-06-11 13:55:25 +0300

    fix buttons and buy nft page

---
+++ components/AmazingButton.vue
@@ -0,0 +1,42 @@
+<template>
+    <div v-if="nonBtn" class="btn flex btn-lg rounded-lg min-h-0 h-auto p-[2px]">
+        <span
+            class="relative w-full p-2 inline-flex flex-row items-center justify-center h-full bg-[rgba(12,12,12,0.95)] rounded-md min-h-full ">
+            <slot />
+        </span>
+    </div>
+    <button v-else class="btn flex btn-lg rounded-lg min-h-0 h-auto p-[2px]">
+        <span
+            class="relative w-full p-2 inline-flex flex-row items-center justify-center h-full bg-[rgba(12,12,12,0.95)] rounded-md min-h-full ">
+            <slot />
+        </span>
+    </button>
+</template>
+<script setup>
+let { nonBtn } = defineProps(['non-btn'])
+</script>
+<style scoped>
+.btn {
+    background-color: #FF8400;
+    background: radial-gradient(circle,
+            #FF0060,
+            #FE6244, #FF8400, #3F0071);
+    background-size: 300% 300%;
+    animation: amazing-transform 20s ease-in-out infinite;
+
+}
+
+@keyframes amazing-transform {
+    0% {
+        background-position: 0 150%;
+    }
+
+    50% {
+        background-position: 250% 0%;
+    }
+
+    100% {
+        background-position: 0 150%;
+    }
+}
+</style>
--- plugins/arweave.js
+++ plugins/arweave.js
@@ -4,7 +4,7 @@ import { useArweave } from "../composables/useState";
 export default async function setArweave() {
     const arweave = useArweave();
     arweave.value = Arweave.init({
-        host: "prophet.rareweave.store",
+        host: "g8way.io",
         port: 443,
         protocol: "https",
         timeout: 60_000,
