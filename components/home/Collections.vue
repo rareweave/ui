eca509377051c35e3c9a2da6d577bcc0eca8fdd1
Sh1Tt <info@mauricestolk.nl>
2023-07-03 22:37:13 +0200

    Update colors

--- components/home/Collections.vue
+++ components/home/Collections.vue
@@ -9,25 +9,30 @@
                     Explore recently created collections on the RareWeave market.
                 </p>
             </div>
-            <div class="relative flex flex-col justify-start items-center w-full h-full mx-auto rounded-[1.5rem] mt-0 pt-4 pb-8 max-w-[85vw] bg-[rgba(17,23,32,1)] text-xl --observe">
+            <div class="relative flex flex-col justify-start items-center w-full h-full mx-auto rounded-[1.5rem] mt-4 pt-4 pb-8 max-w-[85vw] bg-[rgba(17,23,32,1)] text-xl --observe">
                 <div class="relative flex w-full flex-wrap text-[rgba(221, 232, 255, 0.33)] py-2 px-16 border-b-2 border-gray-800">
-                    <span class="font-bold w-1/2 min-w-[200px]">
+                    <span class="font-bold w-1/2 min-w-[200px] underline underline-offset-1">
                         Collection
                     </span>
-                    <span class="w-1/2 min-w-[200px]">
+                    <span class="w-1/2 min-w-[200px] underline underline-offset-1">
                         Quantity
                     </span>
                 </div>
-                <NuxtLink v-if="!isLoading.collections" v-for="collection of collections" :key="collection.id"
+                <NuxtLink v-if="!isLoading.collections" v-for="(collection,i) of collections" :key="collection.id"
                     class="relative flex w-full flex-wrap hover:bg-gray-800 py-2 px-16 border-b-2 border-gray-800" :to="`/collection/${collection.id}`">
-                    <span class="Amazing-text font-bold w-1/2 min-w-[200px]">
+                    <span :class="`Amazing--${['green','red'][i % 2]}`" class="font-bold w-1/2 min-w-[200px]">
                         {{ collection.state.name }}
                     </span>
                     <span class="font-semibold w-1/2 min-w-[200px]">
                         {{ collection.state.items?.length }}
                     </span>
                 </NuxtLink>
-
+            </div>
+            <div class="relative flex flex-col justify-start items-center w-1/2 min-w[316px] max-w-[85vw] h-max mx-auto rounded-[1.5rem] mt-0 py-2 px-4 max-w-[85vw] text-lg mt-16 text-center text-gray-500 --observe">
+                <p>
+                    Please note that on the RareWeave market a &apos;collection&apos; is defined as: &apos;A group of NFTs that a user has categorized together&apos;, 
+                    rather then: &apos;A group of NFTs that share the same contract&apos;.
+                </p>
             </div>
         </div>
     </div>
