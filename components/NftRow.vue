258e4c68f5a4abcb7102e44eef3a11aef14b2a55
Raddeonn <radeonchik.new@gmail.com>
2023-12-09 15:46:10 +0200

    refactor nft row component

--- components/NftRow.vue
+++ components/NftRow.vue
@@ -1,58 +1,53 @@
 <template>
-  <div class="Component">
-    <NuxtLink :to="'/nft/' + nft.id" class="Link">
+  <div class="flex flex-row justify-start bg-[#11172080] items-start w-full">
+    <NuxtLink :to="'/nft/' + nft.id" class="relative flex flex-row justify-start items-center w-full h-full p-1 border-l border-b border-gray-800">
       <Graphic :nft="nft" />
-      <div class="Info">
-        <label class="title"> Name: </label>
-        <span class="title">
+      <div class="grid grid-cols-[186px_1fr] w-full h-[calc(100%-1rem*2)] m-[1rem_0] p-[0_1rem]">
+        <label> Name: </label>
+        <span>
           {{ nft.state.name }}
         </span>
 
-        <label class="description"> Description: </label>
-        <span class="description">
+        <label> Description: </label>
+        <span>
           {{ nft.state.description }}
         </span>
 
-        <label class="owner"> Owner: </label>
-        <span class="owner">
+        <label> Owner: </label>
+        <span>
           {{ nft.state.owner }}
         </span>
 
-        <label class="minter"> Minter: </label>
-        <span class="minter">
+        <label> Minter: </label>
+        <span>
           {{ nft.state.minter }}
         </span>
 
-        <label class="forsale"> For Sale: </label>
-        <span class="forsale">
+        <label> For Sale: </label>
+        <span>
           {{ nft.state.forSale ? "Yes" : "No" }}
         </span>
 
-        <label class="chain"> Chain: </label>
-        <span class="chain">
+        <label> Chain: </label>
+        <span>
           {{ nft.state.listingChain ? nft.state.listingChain : "Arweave" }}
         </span>
 
-        <label class="price"> Price </label>
-        <span class="price" v-if="nft.state.price != null">
+        <label> Price </label>
+        <span v-if="nft.state.price != null">
           {{
             nft.state.price > 0
               ? (
                   Big(nft.state.price) /
                   Big(Coins.Exponents[nft.state.listingCoin])
                 ).toString() +
-                (nft.state.listingCoin ? nft.state.listingCoin : "AR")
+                (nft.state.listingCoin ?? "AR")
               : "free"
           }}
         </span>
 
-        <label class="royalty"> Royalty: </label>
-        <span class="royalty"> {{ nft.state.royalty }} % </span>
-
-        <label class="reservationBlockheight"> Reservation bh: </label>
-        <span class="reservationBlockheight">
-          {{ nft.state.reservationBlockheight }}
-        </span>
+        <label> Royalty: </label>
+        <span> {{ nft.state.royalty }} % </span>
       </div>
     </NuxtLink>
   </div>
@@ -64,45 +59,3 @@ import Coins from "../config/coins";
 import Graphic from "./Graphic.vue";
 const { nft } = defineProps(["nft"]);
 </script>
-<style scoped>
-.Component {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: flex-start;
-  background: rgba(17, 23, 32, 0.5);
-  width: 100%;
-}
-
-.Link {
-  position: relative;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  border-bottom: 1px solid rgba(172, 172, 172, 0.5);
-  border-left: 1px solid rgba(172, 172, 172, 0.5);
-  padding: 1rem;
-}
-
-.Imagewrapper {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-  width: 316px;
-  aspect-ratio: 1/1;
-}
-
-.Info {
-  display: grid;
-  grid-template-columns: 186px 1fr;
-  width: 100%;
-  height: calc((100%) - 1rem * 2);
-  margin: 1rem 0;
-  padding: 0 1rem;
-}
-</style>
